a720ac8081cf6f2a6eefa8dbcb415f0d43b24a4b
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-05-26 19:28:53 +0530

    helloworld

--- 05-timestamp.sh
+++ 05-timestamp.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+# TIMESTAMP=$(date)
 
-echo "Time is:$TIMESTAMP"
+# echo "Time is:$TIMESTAMP"
+
+START-TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL-TIME=$(($END_TIME-$START-TIME))
+
+echo "script executed in $TOTAL_TIME seconds"
