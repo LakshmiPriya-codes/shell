0fb8188cd2e1b859b4b1a2cb99443a912e07068d
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-05-31 21:03:01 +0530

    helloworld

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -13,4 +13,4 @@ sleep 5 &
 echo "PID of background running just now: $!"
 wait &!
 echo "Line Number: $LINENO"
-echo "Script execited in $SECONS seconds"
+echo "Script execited in $SECONDS seconds"
