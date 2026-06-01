936ed2535a9d053479ed841fade0177f765646fe
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-01 16:11:03 +0530

    helloworld

---
+++ 08-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER =$1
+
+# gt - greater than
+# lt -less than 
+# eq -equal
+# ne - not equal
+# ge -greater than  or equal
+# le - less than or equal
+
+
+if[$NUMBER -gt 20]
+ echo "Given Number $NUMBER is greater than or equal to 20"
+fi 
