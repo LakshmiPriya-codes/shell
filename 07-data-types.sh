578f6c85aa9b27e6fd94509c4946258b89f79a39
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-01 15:52:58 +0530

    helloworld

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -10,3 +10,6 @@ echo "sum is: $SUM"
 #Array
 MOVIES=("RRR" "Varanasi" "Pushpa") #index always starts from 0
 echo "Movies are: ${MOVIES[@]}"
+echo "Frist Movie is: ${MOVIES[0]}"
+echo "second Movie is: ${MOVIES[1]}"
+echo "Third Movie is: ${MOVIES[2]}"
