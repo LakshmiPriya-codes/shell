e0fbb4bdaed8a5f46a2ee06b14e33f042bc4985e
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-05 16:18:02 +0530

    helloworld

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
  USERID=$(id -u)
- LOGS_DIR=/home/ec2-user/shell-logs
- LOGS_FILE="$LOGS_DIR/$0.log" # home/ec2-user/shell-logs/10-logs.sh
-
-
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script with root access"
     exit 1
@@ -12,10 +8,10 @@ fi
 
 validate(){
     if [ $2 -ne 0 ]; then
-     echo "Installing $1 is....failed" | tee -a $LOGS_FILE
+     echo "Installing $1 is....failed" 
      exit 1
   else
-      echo "Installing $1 is...Success" | tee -a $LOGS_FILE
+      echo "Installing $1 is...Success" 
   fi  
 }
 
@@ -24,7 +20,7 @@ validate(){
  for package in $@ 
  do 
      echo "Insatlling $package"
-     dnf list installed $package
+     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then 
      dnf install $package -y &>> $LOGS_FILE
      validate "Installing $package" $?
