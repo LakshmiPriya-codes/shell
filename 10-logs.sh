687f043435eb9751abeaacd10767484d2956c359
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-05 20:00:03 +0530

    helloworld

--- 10-logs.sh
+++ 10-logs.sh
@@ -5,7 +5,7 @@
 #second argument --> exit code
  
  USERID=$(id -u)
- LOGS_DIR=/var/log/shell-practice
+ LOGS_DIR=/var/log/shell-script
  LOGS_FILE="$LOGS_DIR/$0.log" # home/ec2-user/shell-logs/10-logs.sh
 
 validate(){
