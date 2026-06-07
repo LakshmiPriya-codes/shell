a0ac93f7676795d4189e4e84a7176e387eca90a5
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-07 14:44:28 +0530

    helloworld

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,7 +3,7 @@
  USERID=$(id -u)
  LOGS_DIR=/var/log/shell-script
  LOGS_FILE="$LOGS_DIR/$0.log" 
- TIME-STAMP=$(date "%Y-%m-%d %H:%M-%S")
+ TIME-STAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script with root access"
