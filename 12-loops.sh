c7c2e70b518f3e3bf9d97b0063f3fca0c335ab29
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-07 14:49:20 +0530

    helloworld

--- 12-loops.sh
+++ 12-loops.sh
@@ -12,10 +12,10 @@ fi
 
 validate(){
     if [ $2 -ne 0 ]; then
-     echo TIMESTAMP [Error] "Installing $1 is....failed" | tee -a $LOGS_FILE
+     echo "TIMESTAMP [Error] Installing $1 is....failed" | tee -a $LOGS_FILE
      exit 1
   else
-      echo TIMESTAMP [Info] "Installing $1 is...Success" | tee -a $LOGS_FILE
+      echo "TIMESTAMP [Info] "Installing $1 is...Success" | tee -a $LOGS_FILE
   fi  
 }
 
@@ -29,7 +29,7 @@ validate(){
      dnf install $package -y &>> $LOGS_FILE
      validate "Installing $package" $?
      else
-       echo TIMESTAMP [Info] "$package already installed.....Skipping"
+       echo "TIMESTAMP [Info] $package already installed.....Skipping"
     fi    
  done
      
