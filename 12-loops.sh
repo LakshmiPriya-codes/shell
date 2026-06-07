7f851937272acf0a01dc02c609abdcec95e21d62
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-07 14:40:36 +0530

    helloworld

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,6 +3,7 @@
  USERID=$(id -u)
  LOGS_DIR=/var/log/shell-script
  LOGS_FILE="$LOGS_DIR/$0.log" 
+ TIME-STAMP=$(date "%Y-%m-%d %H:%M-%S")
 
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script with root access"
@@ -11,10 +12,10 @@ fi
 
 validate(){
     if [ $2 -ne 0 ]; then
-     echo "Installing $1 is....failed" 
+     echo TIME-STAMP [Error] "Installing $1 is....failed" | tee -a $LOGS_FILE
      exit 1
   else
-      echo "Installing $1 is...Success" 
+      echo TIME-STAMP [Info] "Installing $1 is...Success" | tee -a $LOGS_FILE
   fi  
 }
 
@@ -28,7 +29,7 @@ validate(){
      dnf install $package -y &>> $LOGS_FILE
      validate "Installing $package" $?
      else
-       echo "$package already installed.....Skipping"
+       echo TIME-STAMP [Info] "$package already installed.....Skipping"
     fi    
  done
      
