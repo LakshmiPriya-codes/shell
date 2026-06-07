5d7cc2ebf06b2d50d486e17b225efe17290c1fa1
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-07 16:10:21 +0530

    helloworld

--- 15-set.sh
+++ 15-set.sh
@@ -10,7 +10,7 @@
  Y="\e[33m"
  N="\e[0m"
 
- trap 'echo "error $LINENO", command: "$BASH_COMMAND" '
+ trap 'echo "error $LINENO", command: $BASH_COMMAND" '
 
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script with root access"
