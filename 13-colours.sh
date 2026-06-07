63c8c3a45ed09f91940849275758d1ea3cadd1fb
Lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-07 15:27:45 +0530

    helloworld

--- 13-colours.sh
+++ 13-colours.sh
@@ -5,10 +5,10 @@
  LOGS_DIR=/var/log/shell-script
  LOGS_FILE="$LOGS_DIR/$0.log" 
  TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
- R= "\e[31m"
- G= "\e[32m"
- Y= "\e[33m"
- N= "\e[34m"
+ R="\e[31m"
+ G="\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
 
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script with root access"
