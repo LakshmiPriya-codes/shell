a0aa7bc91640a787d97e4a6b6e669b2169027356
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-14 14:37:40 +0530

    api doc added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -16,5 +16,22 @@ DAYS=${2:-14} #default 14 days
 if [ ! -d $SOURCE_DIR ]; then
     echo "Error directory $source_dir does nort exit"
     exit 1
+fi
+
+
+    echo "Scanning $SOURCE_DIR for log files older than 14 days"
+    FILES=$(find $source_dir -name '*.log' -type f -mtime +$DAYS)
+
+if [ -z $FILES ]
+
+    echo "No log files older than 14 days found"
+    exit 0
 fi    
+ 
+ while IFS= read -r FILE
+ do 
+    echo " File to be deleted $FILE "
+ done <<< "$FILES"
+
+
 
