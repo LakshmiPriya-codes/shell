9318d17e076815da0573ebe5153e87a90c1a306f
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-14 14:43:12 +0530

    api doc added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -5,8 +5,8 @@ DAYS=${2:-14} #default 14 days
 
 
  if [ -z $SOURCE_DIR ]; then
-    echo "ERROR: Missing parameters"
-    echo "USAGE: $0 <source-dir> [days(optional default 14 days)] "
+    echo "ERROR:: Missing parameters"
+    echo "USAGE:: $0 <source-dir> [days(optional default 14 days)] "
     exit 1
 
  fi   
@@ -14,12 +14,12 @@ DAYS=${2:-14} #default 14 days
 
      
 if [ ! -d $SOURCE_DIR ]; then
-    echo "Error directory $source_dir does nort exit"
+    echo "Error directory: $source_dir does nort exit"
     exit 1
 fi
 
     echo "Scanning $SOURCE_DIR for log files older than 14 days"
-    FILES=$(find $source_dir -name '*.log' -type f -mtime +$DAYS)
+    FILES=$(find $source_dir -name '*.log' -type f -mtime  +$DAYS)
 
 if [ -z "$FILES" ]; then
     echo "No log files older than 14 days found"
