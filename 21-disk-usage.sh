04864b3a2665146f74c9c77756cf15637267d439
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-15 16:48:16 +0530

    api doc added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THERSHOLD=10
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
@@ -13,3 +14,7 @@ do
 done <<< "$DISK_USAGE"
 
 echo  -e "$MESSAGE"
+
+sh mail.sh "Devops Team" "High disk usage" "$SERVER_IP" "$MESSAGE" "priyavempalli95@gmail.com" "High Disk Usage alert"
+
+
