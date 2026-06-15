632f1d1cde834cbc31c0e976f474d7362920950a
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-15 15:56:18 +0530

    api doc added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -5,7 +5,7 @@ USAGE_THERSHOLD=10
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" f1)
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$(echo $line | awk '{print $7}' )
     if [ $USAGE -ge "$USAGE_THERSHOLD" ]; then 
         MESSAGE=" High disk usage on $PARTITION: $USAGE"
